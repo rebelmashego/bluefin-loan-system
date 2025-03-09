26737b88598670be8afd7e0067ce278d22c0d87e
rebelmashego <rebelmashego2@gmail.com>
2025-03-09 09:36:43 +0200

    Create bluefin-theme/index.php

---
+++ bluefin-theme/index.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+<h1>Welcome to Bluefin Payday Loans</h1>
+<p>Fast, secure, and hassle-free loans.</p>
+<a href="<?php echo site_url('/apply-now'); ?>">Apply Now</a>
+<?php get_footer(); ?>
